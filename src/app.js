3d511ecef9ecc32861d8cf926c82a5d927165c57
David <dnaranjo0@misena.edu.co>
2022-05-30 20:37:00 -0500

    start proyect

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const express = require("express")
-const path = require('path');
-const publicPath = path.resolve(__dirname,'../public');
 const app = express()
 const index_routes = require("./routers/index.routes")
 
-app.use(express.static(publicPath));
 
 app.use('/', index_routes);
 
