f24301a3db370f22b5a89fe57a785c9b94796c73
David <dnaranjo0@misena.edu.co>
2022-10-10 00:48:51 -0500

    home layout

---
+++ src/public/js/homeInteractions.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", ()=>{
+    let locationPage = window.location.pathname
+    if(locationPage === "/"){
+        let titleCode = document.querySelector(".titles-code")
+        let iconCode = document.querySelector(".icons-code")
+        let titleDesing = document.querySelector(".titles-desing")
+        let iconDesing = document.querySelector(".icons-desing")
+        titleCode.addEventListener("click", ()=>{
+            iconCode.classList.add("show-icons")
+            titleCode.classList.add("selected")
+            iconDesing.classList.remove("show-icons")
+            titleDesing.classList.remove("selected")
+        })
+        titleDesing.addEventListener("click", ()=>{
+            iconDesing.classList.add("show-icons")
+            titleDesing.classList.add("selected")
+            iconCode.classList.remove("show-icons")
+            titleCode.classList.remove("selected")
+        })
+    }   
+})
