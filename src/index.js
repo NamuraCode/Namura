a01e1af66c0f4faab9501a36381fb8a8027d634c
David <dnaranjo0@misena.edu.co>
2023-03-22 22:53:32 -0500

    pull master

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set('views', join(__dirname, './views'))
 // Template ejs install
 app.set('view engine', 'ejs')
-
 app.listen(3000,()=>{ 
     console.log("runnig in port 3000")
 })
