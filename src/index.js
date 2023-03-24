296cc7e51c5fa128ca3b7b03b041bf108fb7729f
David Naranjo <naranjo.david.1999@gmail.com>
2023-03-23 20:04:09 -0500

    import enviroment variable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import * as dotenv from 'dotenv'
+dotenv.config()
 const __dirname = dirname(fileURLToPath(import.meta.url));
 // Absolute path views for partials
 app.set('views', join(__dirname, './views'))
 // Template ejs install
 app.set('view engine', 'ejs')
-app.listen(3000,()=>{ 
+app.listen( process.env.PORT || 3001,()=>{ 
     console.log("runnig in port 3000")
 })
