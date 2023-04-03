e9587616a55bc65f7cd1ea5e61c3d5b5cb61c0ed
David <dnaranjo0@misena.edu.co>
2023-04-02 22:47:17 -0500

    pague iteractions

--- src/public/js/paguesInteractions.js
+++ src/public/js/paguesInteractions.js
@@ -5,20 +5,6 @@ window.addEventListener("load", ()=>{
             const pageItems = document.querySelectorAll(".info-content-pages")
             const decrementArrow = document.querySelector(".change-left")
             const increaseArrow = document.querySelector(".change-right")
-            let showCart = Math.trunc(pageItems.length / 2)
-            const minPositionCarts =  -120
-            const cartPositions = [
-                `translate(${minPositionCarts / 1}%, 0px) scale(0.4)`,
-                `translate(${minPositionCarts / 1.7}%, 0px) scale(0.6)`,
-                `translate(${minPositionCarts - minPositionCarts}%, 0px) scale(1)`,
-                `translate(${(minPositionCarts / 1.7) * -1}%, 0px) scale(0.6)`,
-                `translate(${(minPositionCarts / 1) * -1}%, 0px) scale(0.4)`
-            ]
-
-            for (let index = 0; index < pageItems.length; index++) {
-                pageItems[index].style.transform = cartPositions[index]
-            }
-
             function changeCartIndex(operation){
                 operation != "decrement" ? showCart += 1 : showCart -= 1 
                 if (showCart > pageItems.length) {
@@ -30,17 +16,12 @@ window.addEventListener("load", ()=>{
                 }
                 return showCart
             }
-
             decrementArrow.addEventListener("click", ()=>{
                 console.log(changeCartIndex('decrement'))
             })
-
             increaseArrow.addEventListener("click", ()=>{
                 console.log(changeCartIndex('increment'))
             })
-
-
-          
         }
     }catch(error){
         console.log(error);
