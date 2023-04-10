45f81c75cfb3b61adbf58a770406bf8eee2b15af
David <dnaranjo0@misena.edu.co>
2023-04-09 21:36:53 -0500

    slider horizontal

--- src/public/js/paguesInteractions.js
+++ src/public/js/paguesInteractions.js
@@ -2,26 +2,6 @@ window.addEventListener("load", ()=>{
     let locationPage = window.location.pathname
     try{
         if(locationPage === "/pages"){
-            const pageItems = document.querySelectorAll(".info-content-pages")
-            const decrementArrow = document.querySelector(".change-left")
-            const increaseArrow = document.querySelector(".change-right")
-            function changeCartIndex(operation){
-                operation != "decrement" ? showCart += 1 : showCart -= 1 
-                if (showCart > pageItems.length) {
-                    showCart = 1
-                }else{
-                    if (showCart < 1) {
-                        showCart = pageItems.length
-                    }
-                }
-                return showCart
-            }
-            decrementArrow.addEventListener("click", ()=>{
-                console.log(changeCartIndex('decrement'))
-            })
-            increaseArrow.addEventListener("click", ()=>{
-                console.log(changeCartIndex('increment'))
-            })
         }
     }catch(error){
         console.log(error);
